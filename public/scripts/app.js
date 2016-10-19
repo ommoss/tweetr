abbd7d8c88695d662998e764029ddd45ef4dd4ba
Owen Moss <ommoss63@gmail.com>
2016-10-19 20:55:39 +0000

    css styline done for compose, nav-bar, tweets, tweets fetch from database and are displayed chronologically

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,71 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
+function loadTweets(){
+  var tweetData = $.ajax({
+    url: "/tweets/",
+    method: "GET",
+    dataType: 'json'
+  });
+  tweetData.done(function(tweetDataObj){
+    renderTweets(tweetDataObj);
+  })
+}
+
+$(function(){
+   var tweetData = $.ajax({
+    url: "/tweets/",
+    method: "GET",
+    dataType: 'json'
+  });
+  tweetData.done(function(tweetDataObj){
+    for(var i = tweetDataObj.length-1; i >= 0; i--){
+      var $tweet = createTweetElement(tweetDataObj[i]);
+      $('#tweets-container').append($tweet);
+    }
+  });
+});
+
+function clearField() {
+   document.getElementsByName("text")[0].value = "";
+ }
+
+
+function renderTweets(tweets) {
+  var $tweet = createTweetElement(tweets[tweets.length-1]);
+  $('#tweets-container').prepend($tweet);
+}
+
+function createTweetElement(tweet){
+  var date = new Date(tweet.created_at)
+
+  var a = $("<article>").addClass("tweets");
+  var h = $("<header>");
+  var t = $("<h2>")
+  var i = $("<image src =" + tweet.user.avatars.small+">").addClass("logo");
+  var s = $("<span>");
+  var f = $("<footer>");
+  var p = $("<p>");
+  s.append(tweet.user.handle);
+  t.append(tweet.user.name);
+  h.append(i, t, s);
+  f.append(date);
+  p.append(tweet.content.text);
+  a.append(h,p,f);
+  return a;
+};
+
+$(function(){
+  $('#new-tweet').on('submit', function(event){
+    // console.log('gee');
+    event.preventDefault();
+    const thisFrom = $(this);
+    var newTweet = $.ajax({
+      url: thisFrom.attr('action'),
+      method: thisFrom.attr('method'),
+      data: thisFrom.serialize(),
+    });
+    newTweet.done(function(data){
+      loadTweets(data);
+      clearField();
+    });
+  });
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,11 @@
-var i = 140;
-$("textarea").ready(function(){
-  $("input").keypress(function(){
-    $("span").text(i -= 1);
-  });
-});
+document.getElementsByName("text")[0].addEventListener("keyup", countChar);
+
+function countChar(){
+    var counter = 140;
+    counter =  counter - this.value.length;
+    document.getElementsByClassName("counter")[0].innerHTML = counter;
+    document.getElementsByClassName("counter")[0].style.color = "";
+    if(counter < 0){
+      document.getElementsByClassName("counter")[0].style.color = "red";
+    }
+}
